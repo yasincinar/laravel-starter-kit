64f30c71d0fa58635ff2e559f6d9bb7333333ca7
salihklc <salihklc91@gmail.com>
2016-06-29 11:10:19 +0300

    migration done without foreign keys

--- database/migrations/2016_06_28_130126_sentinel_additional_columns.php
+++ database/migrations/2016_06_28_130126_sentinel_additional_columns.php
@@ -13,13 +13,12 @@ class SentinelAdditionalColumns extends Migration
     public function up()
     {
         Schema::table('users', function ($table) {
-            $table->string('slug')->unique()->change();
-            $table->string('email')->unique();
+            $table->string('slug')->unique();
+            $table->integer('city_id')->unsigned()->nullable();
             $table->integer('identity_number');
             $table->integer('cell_phone');
             $table->string('profile_image');
             $table->text('address')->nullable();
-            $table->integer('city_id')->unsigned()->nullable();
 
             $table->foreign('city_id')->references('id')->on('cities')->onDelete('set null');
 
